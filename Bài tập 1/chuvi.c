9d5f6d243f6eb6d7767798643ca650da1350444b
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-11-26 02:03:55 -0800

    Update chuvi.c

--- Bài tập 1/chuvi.c
+++ Bài tập 1/chuvi.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main()
 	{
-  double r,pl,cv,dt;
+  double r,cv,dt;
+  const double pi = 3.14;
   r = 2.5;
-  cv =2*r*3.14;
-  dt =r*r*3.14;
-  printf("cv \t dt \n %0.01f \t %0.01f",cv,dt);
+  cv =2*r*pi;
+  dt =r*r*pi;
+  printf("cv \t dt \n %0.001f \t %0.001f",cv,dt);
   return 0;
   }
