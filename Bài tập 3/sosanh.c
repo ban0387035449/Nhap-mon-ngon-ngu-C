a0d28adcce6cc1b924b411c0a05c738f4244f7d9
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-10 00:44:38 -0800

    Update sosanh.c

--- Bài tập 3/sosanh.c
+++ Bài tập 3/sosanh.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 int main() {
+ 
   int a,b ;
+  // nhập 2 số nguyên a b ngẫu nhiên.
   printf ("nhap so a =");
   scanf("%d",&a);
   printf ("nhap so b = ");
   scanf ("%d",&b);
   if (a > b){
-  printf ("%d a > b", a);
+  printf ("a > b");
   }else if (a == b){
-  printf("%d a = b", a);
+  printf("a = b");
   }else {
-    printf ("%d,a <b", a);
+    printf ("a <b");
   }
   getchar();
   return 0;
