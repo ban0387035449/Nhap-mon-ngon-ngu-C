cc0336724211db68eb61caaefbbc55ca8065bb38
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-17 01:14:02 -0800

    Update pheptinh.c

--- Bài tập 3/pheptinh.c
+++ Bài tập 3/pheptinh.c
@@ -6,10 +6,10 @@ int main(){
   // nhập phép tính cộng hoặc trừ
   scanf ("%c",&pt);
   switch(pt) {
- case '+': //trường hợp dùng dấu " + " máy sẽ thực hiện phép tính a+ b
+ case '+': //trường hợp dùng dấu ' + ' máy sẽ thực hiện phép tính a+ b
      printf (" a + b = %d",a + b);
       break;
- case '-': //trường hợp dùng dấu " - " máy sẽ thực hiện phép tính a -b
+ case '-': //trường hợp dùng dấu ' - ' máy sẽ thực hiện phép tính a -b
      printf (" a - b = %d",a - b);
       break;
  default: // các trường hợp còn lại hiển thị yêu cầu nhập lại phép tính
