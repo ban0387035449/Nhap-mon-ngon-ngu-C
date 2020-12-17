480c5acb64600ec795cd5681f7f1f18f19232e11
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-17 01:46:37 -0800

    Update thang.c

--- Bài tập 3/thang.c
+++ Bài tập 3/thang.c
@@ -15,8 +15,15 @@ int main(){
    case 11 :
      printf (" 30 ngay");
       break;
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
       default: // các trường hợp còn lại
-       printf("30 ngay");
+       printf ( "vui long nhap tu 1 den 12");
     
   }
  
