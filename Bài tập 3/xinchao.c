cf619c13785b56bce31553a204930e818b8cdb59
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-23 23:54:07 -0800

    Update xinchao.c

--- Bài tập 3/xinchao.c
+++ Bài tập 3/xinchao.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-  int count;
-  for (count = 1 ; count <= 10; count++){
+  int count;// khai bao bien count=đếm
+  for (count = 1 ; count <= 10; count++)// lặp lai 10 lần
   printf ("\n xin chao ");
   }
   return 0;
