19d90252860a03c12ad6c716768b810befd14b65
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-23 23:36:51 -0800

    Update tong10.c

--- Bài tập 3/tong10.c
+++ Bài tập 3/tong10.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
  
 int main(){
-    int a = 1;
-    int b = 10;
- 
+   
     int sum = 0; // cần khởi tạo giá trị
     
-    for(int i = a; i <= b; i++){
+    for(int i = 1; i <= 10; i++){
         sum += i; // sum = sum + i
     }
     printf("Sum = %d", sum);
