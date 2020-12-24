373923bdfbe893444d9336b81274b00af68a3f36
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-23 18:34:43 -0800

    xin chao .c
    #include <stdio.h>
    
    int main (vold){
     int dem ;
    for (dem = 1; dem <= 10; dem = dem + 1){
    	// HIEN THI RA MAN HINH 10 LAN   
    printf("Xin chao \ n");
        // Dùng lenh printf de hien ra man hinh dong chu xin chao                                        
    }
    return 0;
    }

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (vold){
+ int dem ;
+for (dem = 1; dem <= 10; dem = dem + 1){
+	// HIEN THI RA MAN HINH 10 LAN   
+printf("Xin chao \ n");
+    // Dùng lenh printf de hien ra man hinh dong chu xin chao                                        
+}
+return 0;
+}
