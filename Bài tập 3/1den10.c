6c28752881454a42714744e48027df7970ea3318
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-23 23:22:39 -0800

    Create 1den10.c

---
+++ Bài tập 3/1den10.c
@@ -0,0 +1,41 @@
+//nằm rên 1 hàng ngang
+#include <stdio.h>
+
+int main()
+{
+  int num;
+  for (num = 1 ; num <= 10; num++){
+  printf ("%d  " , num);
+  }
+  return 0;
+  
+  
+  //nằm trên 10 hàng#include <stdio.h>
+
+ #include <stdio.h>
+
+int main()
+{
+  int num;
+  for (num =1 ; num <= 10; num++){
+  printf(" %d \n",num);
+  }
+  return 0;
+}
+
+// vừa có ngang vừa có dọc
+#include <stdio.h>
+
+int main()
+{
+  int num;
+  for (num =1 ;num <= 10; num++){
+    printf(" %d ",num  );
+    }
+  
+  for  ( num =2   ; num <= 10; num++){
+   printf("\n %d ",num);
+  }
+  
+  return 0;
+}
