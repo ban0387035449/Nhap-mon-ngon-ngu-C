f25d64de75007974aaa417bbf795cfed5e5ab2ba
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-28 18:45:42 +0700

    Update amduong.c

--- Bài tập 3/amduong.c
+++ Bài tập 3/amduong.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  int a ;
-  printf ("nhap so a =");
-  scanf ("%d",&a);
-  if (a > 0){
-  printf (" %d duong", a);
-  }else if (a == 0){
-  printf("%d = 0", a);
-  }else {
-    printf ("%d,am", a);
+  int a ;//khai bao bien a
+  printf ("nhap so a =");//hien thi nguyen a
+  scanf ("%d",&a);//nhap so nguyen a
+  if (a > 0){ //truong hop a>0 thi
+  printf (" %d duong", a);//hien thi a duong
+  }else if (a == 0){// truong hop a =0 
+  printf("%d = 0", a);// hien thi a = 0
+  }else { //cac truong hop con lai
+    printf ("%d,am", a); //hien thi a am
   }
   getchar();
   return 0;
