60c5590be30739dcaae410154ffb5a9e014d7943
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-31 19:06:02 +0700

    Create review_01.c

---
+++ Bài tập 3/review_01.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#include <math.h>
+
+int main() {
+int n;
+printf("Nhap so n: ");
+  scanf("%d", &n);
+ if (n==1 ){   
+ printf("\n one ");
+} 
+else if (n==2 ){   
+ printf("\n two");
+} 
+else if (n==3 ){   
+ printf("\n three ");
+} 
+else if (n==4 ){   
+ printf("\n four ");
+} 
+else if (n==5 ){   
+ printf("\n five ");
+} 
+else if (n==6 ){   
+ printf("\n six ");
+} 
+else if (n==7 ){   
+ printf("\n seven ");
+} 
+else if (n==8 ){   
+ printf("\n eight ");
+} 
+else if (n==9 ){   
+ printf("\n nice ");
+} 
+else
+printf("\n vui long nhap tu 1 den 9");
+}
