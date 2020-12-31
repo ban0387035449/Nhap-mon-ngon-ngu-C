488cf817049c275105873a661da7d170bc48ac1e
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-31 14:58:32 +0700

    Create kt08.c

---
+++ Bài tập 3/kt08.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+ int main(){
+ 	float a,b;
+ 	printf (" nhap so a :");
+ 	scanf ( "%f",&a);
+ 	printf (" nhap so b :");
+ 	scanf ( "%f",&b);
+ 	if (a >0 && b>0){
+     printf ("a va b cung dau");}
+  else	if (a <0 && b<0){
+     printf ("a va b cung dau");}
+  else	if (a==0 && b ==0){
+     printf ("a va b cung dau");}
+  else  {
+	 
+ 	printf (" a va b khac dau");}
+ }
