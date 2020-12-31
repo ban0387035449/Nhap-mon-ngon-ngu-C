8d177d5e5edcf1285b64625a9866742080be9472
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-31 22:22:23 +0700

    Update kt04.c

--- Bài tập 3/kt04.c
+++ Bài tập 3/kt04.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(){
-	int s, n, sum = 0;
-  
-	printf("Nhap so n:");
-  	scanf("%d",&n);
-	for (s=1; s <= n; s++){
-		sum += s;}
-    printf("tong=%d",sum);
-   }
+    int n; //so dc nhap
+    int i;//số được tách ra sau khi nhập
+    int s = 0; 
+    printf("Nhap n: ");
+    scanf("%d",&n);
+    for(;n!=0;){ //n khac 0 bat dau vong lap
+        i = n % 10; //chia 10 để lấy phần dư 
+        s += i;     // s +=i   0+ phan du
+        n /= 10;     // n=n/10
+    }    
+    printf("%d",s); //hien thi ket qua sau qua trinh lap
+}
