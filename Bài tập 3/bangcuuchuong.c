2f5913ea1a2f8928cdace29d4d823f9e4654e871
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-31 10:28:23 +0700

    Create bangcuuchuong.c

---
+++ Bài tập 3/bangcuuchuong.c
@@ -0,0 +1,11 @@
+int main(){
+    int a,b;
+    printf ("bang cuu chuong ");
+   
+    for(int a = 1; a <= 10;a++){
+        printf("\n cuu chuong %d \n", a);
+        for(int j = 0; j < 10; j++){
+            printf("\t%d x %d = %d\n", a, b, a*b);
+        }
+    }
+}
