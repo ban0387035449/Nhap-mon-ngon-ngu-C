19f9a2c991d1294d51b4846b3024e3a45b977f9b
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-31 15:34:49 +0700

    Update kt01.c

--- Bài tập 3/kt01.c
+++ Bài tập 3/kt01.c
@@ -3,7 +3,7 @@ int main(){
     int a,b;
     printf ("bang cuu chuong ");
    
-    for(int a = 1; a <= 10;a++){
+    for(int a = 1; a < 10;a++){
         printf("\n cuu chuong %d \n", a);
         for(int b = 1; b <= 10; b++){
             printf("\t%d x %d = %d\n", a, b, a*b);
