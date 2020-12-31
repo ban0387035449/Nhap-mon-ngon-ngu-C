211a09ac02204262916f490e86193d1bd06731b6
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-31 18:35:49 +0700

    Update kt07.c

--- Bài tập 3/kt07.c
+++ Bài tập 3/kt07.c
@@ -3,21 +3,19 @@
 #include <math.h>
 
 int main() {
-           float a, b, c;
-          printf("Nhap canh thu nhat: ");
-          scanf("%f", &a);
+float a, b, c;
+printf("Nhap canh thu nhat: ");
+  scanf("%f", &a);
 
-          printf("Nhap canh thu hai: ");
-          scanf("%f", &b);
+ printf("Nhap canh thu hai: ");
+ scanf("%f", &b);
 
-          printf("Nhap canh thu ba: ");
-          scanf("%f", &c);
+ printf("Nhap canh thu ba: ");
+ scanf("%f", &c);
 
-          if((a+b>c) && (a+c>b) && (c+b >a)) {
-
-                   printf("\nDay la ba canh cua tam giac");
-          } else
-
-                   printf("\nDay khong phai la ba canh cua tam giac");
-
+ if((a+b>c) && (a+c>b) && (c+b >a)) {
+   
+printf("\nDay la ba canh cua tam giac");
+} else
+printf("\nDay khong phai la ba canh cua tam giac");
 }
