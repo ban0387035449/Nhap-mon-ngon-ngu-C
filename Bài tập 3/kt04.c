0aaed714929dfca5150f441233a5c62758d4fe7b
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2020-12-31 15:07:48 +0700

    Update kt04.c

--- Bài tập 3/kt04.c
+++ Bài tập 3/kt04.c
@@ -4,12 +4,7 @@ int main(){
   
 	printf("Nhap so n:");
   	scanf("%d",&n);
-  
-	printf("n = %d\n",n);
-  
 	for (s=1; s <= n; s++){
-		sum += s;
-
-	}
-		printf("tong=%d",sum);
-}
+		sum += s;}
+    printf("tong=%d",sum);
+   }
