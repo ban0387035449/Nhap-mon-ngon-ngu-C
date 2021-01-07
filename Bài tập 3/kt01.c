c8f43d977362355a7948c1003fe8db11ef30a339
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2021-01-07 16:41:23 +0700

    Update kt01.c

--- Bài tập 3/kt01.c
+++ Bài tập 3/kt01.c
@@ -10,3 +10,13 @@ int main(){
         }
     }
 }
+#include <stdio.h>
+int main(){
+int a=1,b=2,c=3,d=4;
+printf("cuu chuong 2\n");
+printf("2*1=%d \n2*2=%d \n2*3=%d\n",b,b*b,b*c);
+printf("cuu chuong 3\n");
+printf("3*1=%d \n3*2=%d \n3*3=%d\n",c,c*b,c*c);
+printf("cuu chuong 4\n");
+printf("4*1=%d \n4*3=%d \n4*3=%d",d,d*b,d*c);
+}
