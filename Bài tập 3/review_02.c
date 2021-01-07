97ec30d164e4c1f342efd459a909c8300c49cd5c
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2021-01-07 15:48:36 +0700

    Update review_02.c

--- Bài tập 3/review_02.c
+++ Bài tập 3/review_02.c
@@ -1,52 +1,50 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
 
 int main(){
-	int a,b,n;
-
-	printf("Nhap a: ");
-	scanf("%d",&a);
-	printf("Nhap b: ");
-	scanf("%d",&b);
-  
-	for (n=a;n<=b;n++){
-	switch (n){
-    case 1: //nhap n=1
-       printf("one\t");
-	   break;
-	case 2: //nhap n=2
-	   printf("two\t");
-	   break;
-	case 3: //nhap n=3
-	   printf("three\t");
-	   break;
-    case 4: //nnhap n=4
-	   printf("four\t");
-	   break;
-	case 5: //nhap n=5
-	   printf("five\t");
-	   break;
-	case 6: //nhap n=6
-	   printf("six\t");
-	   break;
-	case 7: //nhap n=7
-	   printf("sevent\t");
-	   break;
-	case 8: //nhap n=8
-	   printf("eight\t");
-	   break;
-	case 9: //nhap n=9
-	   printf("nine\t");
-	   break;
-    default: //neu n>9
-    if (n%2==0){ //n la so chan
-	   printf("even\t");
-	   }
-	else{ //n la so le
-	    printf("odd\t");
-	}
-	}
-	}
-
-	
-	return 0;
+    int a,b,n;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    for ( n=a;n<=b;n++){
+    switch (n){
+    case 1: 
+       printf("one\n");
+       break;
+    case 2: 
+       printf("two\n");
+       break;
+    case 3:
+       printf("three\n");
+       break;
+    case 4: 
+       printf("four\n");
+       break;
+    case 5: 
+       printf("five\n");
+       break;
+    case 6: 
+       printf("six\n");
+       break;
+    case 7: 
+       printf("seven\n");
+       break;
+    case 8: 
+       printf("eight\n");
+       break;
+    case 9: 
+       printf("nine\n");
+       break;
+    default: 
+    if (n%2==0){ 
+       printf("even\n");
+       }
+    else{ 
+        printf("odd\n");
+    }
+    }
+    }
 }
