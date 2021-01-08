3ef31a61da767835a3752bea40639cf657456446
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2021-01-08 10:42:26 +0700

    Update kt03.c

--- Bài tập 3/kt03.c
+++ Bài tập 3/kt03.c
@@ -1,13 +1,57 @@
-#include<stdio.h>
-int main(){
-    int a;
-for (a=1;a<=4;a+=1){
-  printf("\t%d",a);
+#include <stdio.h>
+
+int main() {
+   int i;
+
+   printf("In cac so le tu 1 den 20:\n");
+   for(i = 1; i <= 4; i++) {
+      if(i%2 != 0)
+         printf("%d\n", i);
+   }
+
+    for(i = 6; i <= 10; i++) {
+     if(i%2 != 0)
+         printf("%d\n", i);
+   }
+   for(i = 12; i <= 14; i++) {
+      if(i%2 != 0)
+         printf("%d\n", i);
+   }
+   for(i = 16; i <= 20; i++) {
+      if(i%2 != 0)
+         printf("%d\n", i);
+   }
+     return 0;
 }
-for (a=6;a<=10;a+=1){
-  printf("\t%d",a);
+#include <stdio.h>
+
+int main() {
+   int i;
+
+   printf("In cac so le tu 1 den 20:\n");
+   ( i%2 != 0 );
+    for(i = 1; i <= 20; i++) {
+    
+      if( i==5 || i==11||i==15)
+      continue;
+         printf("%d\n", i);
+   
+   }
+     return 0;
 }
-for (a=12;a<=20;a+=1){
-  printf("\t%d",a);
-}
+#include <stdio.h>
+
+int main() {
+   int i;
+
+   printf("In cac so le tu 1 den 20:\n");
+   ( i%2 != 0 );
+    for(i = 1; i <= 20; i++) {
+    
+      if( i==5 || i==11||i==15)
+      continue;
+         printf("%d\n", i);
+   
+   }
+     return 0;
 }
