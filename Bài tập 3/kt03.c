3b6a634def1bb1eba6aca9b113bc2127d2c361a1
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2021-01-08 10:11:06 +0700

    Update kt03.c

--- Bài tập 3/kt03.c
+++ Bài tập 3/kt03.c
@@ -1,25 +1,13 @@
-#include <stdio.h>
-
-int main() {
-   int i;
-
-   printf("In cac so le tu 1 den 20:\n");
-   for(i = 1; i <= 4; i++) {
-      if(i%2 != 0)
-         printf("%d\n", i);
-   }
-
-    for(i = 6; i <= 10; i++) {
-     if(i%2 != 0)
-         printf("%d\n", i);
-   }
-   for(i = 12; i <= 14; i++) {
-      if(i%2 != 0)
-         printf("%d\n", i);
-   }
-   for(i = 16; i <= 20; i++) {
-      if(i%2 != 0)
-         printf("%d\n", i);
-   }
-     return 0;
+#include<stdio.h>
+int main(){
+    int a;
+for (a=1;a<=4;a+=1){
+  printf("\t%d",a);
+}
+for (a=6;a<=10;a+=1){
+  printf("\t%d",a);
+}
+for (a=12;a<=20;a+=1){
+  printf("\t%d",a);
+}
 }
