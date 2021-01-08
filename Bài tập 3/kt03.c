3d66793a73e2e793f88aa0cb985ff59e5526c53a
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2021-01-08 14:50:59 +0700

    Update kt03.c

--- Bài tập 3/kt03.c
+++ Bài tập 3/kt03.c
@@ -21,12 +21,19 @@ int main() {
    int i;
 
    printf("In cac so le tu 1 den 20:\n");
-    for(i = 1; i <= 20; i++) {
-    if( i%2 != 0 ){
-      if( i==5 || i==11||i==15)
-      continue;
-         printf("%d\n", i);
+    i=1;
+    while (i < 20){
+       i++;
+    if ( i%2){
+     switch(i){
+       case 5:
+       case 11:
+       case 15:
+       break;
+     default:
+         printf("%d\n",i);
+     }
     }
-   }
+    } 
      return 0;
 }
