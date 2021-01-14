aad27a686ea014e09fb3c23686b7bcb5f3c5ee84
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2021-01-14 14:15:15 +0700

    Create chuthich.c

---
+++ Bài tập 4/chuthich.c
@@ -0,0 +1,12 @@
+cấu tạo của mảng
+1 khai báo biến{0, 1,  2, 3,  4}vị trí các số ở mảng dưới
+  int arr[5] = {4, 12, 7, 15, 9};
+kiểu dữ liệu, tên mảng[số phần tử của mảng]={các phần tử cách nhau bởi dấu "phẩy'}
+2 vòng lặp trong mảng
+for(int idx=0; idx<5; idx++)//idx là vị trí phần tử của mảng 
+*vd thay bảng 3 hiển thị =15 
+3 in các thành phần trong mảng
+       { 
+        printf("%d\n", arr[idx]);
+    }
+    
