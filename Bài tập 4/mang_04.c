a434c6714908710bf57dbe148bd880659e3f3878
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2021-01-21 14:35:48 +0700

    Update mang_04.c

--- Bài tập 4/mang_04.c
+++ Bài tập 4/mang_04.c
@@ -3,23 +3,23 @@
 int main(void) {
   int n,tong=0;
   //nhap so phan tu cua mang a
-  printf("nhap n= ");
-  scanf("%d",&n);
+     printf("nhap n= ");
+     scanf("%d",&n);
   //khai bao mang a
   int a[n];
   //vong lap cua mang a chay tu 0 den n-1
   for(int i=0;i < n ;i++){
-    printf("nhap phan tu thu %d: ",i+1);
-        scanf("%d", &a[i]);
+     printf("nhap phan tu thu %d: ",i+1);
+     scanf("%d", &a[i]);
   }
-    printf("cac phan tu cua mang a: ");
+     printf("cac phan tu cua mang a: ");
   for(int i=0; i < n; i++){
     //hien thi cac phan tu cua mang a
       printf(" %d",a[i]);
       //tinh tong cua cac phan tu trong mang
-      tong+=a[i];
+   tong+=a[i];
       
       }
   
-  printf("\n tong cac phan tu cua mang %d",tong);
-}
+      printf("\n tong cac phan tu cua mang %d",tong);
+  }
