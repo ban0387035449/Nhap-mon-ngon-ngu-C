814752460189063b46cbc1e1557c6ad4a874655f
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2021-01-21 16:57:22 +0700

    Create mang_06.c

---
+++ Bài tập 4/mang_06.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int main(void) {
+  int i, n,m,trung_gian=0;
+    printf("nhap n =");
+    scanf("%d",&n);
+ int a[n];
+ for( i=0;i<n;i++){#include <stdio.h>
+
+int main(void) {
+  int i, n,m,trung_gian=0;
+    printf("nhap n =");
+    scanf("%d",&n);
+ int a[n];
+ for( i=0;i<n;i++){
+    printf("nhap phan tu thu %d =",i+1);
+    scanf("%d",&a[i]);
+ }
+    printf("cac phan tu cua mang a :");
+ for(int i=0;i<n;i++){
+    printf("%d ",a[i]);
+ }
+   
+	printf("\n Nhap so m can kiem tra: ");scanf("%d",&m);
+ 
+	for (i=0; i<n; i++){
+		if (a[i]==m){
+			trung_gian+=1;
+		}	
+	}
+	printf("\n có %d phần tử giong",trung_gian);
+    printf("\n %d",a[i]);
+	if (trung_gian > 0){
+		printf("\n Co ton tai");
+	}
+	else{
+		printf("Khong ton tai");
+	}
+}
+    printf("nhap phan tu thu %d =",i+1);
+    scanf("%d",&a[i]);
+ }
+    printf("cac phan tu cua mang a :");
+ for(int i=0;i<n;i++){
+    printf("%d ",a[i]);
+ }
+   
+	printf("\n Nhap so m can kiem tra: ");scanf("%d",&m);
+ 
+	for (i=0; i<n; i++){
+		if (a[i]==m){
+			trung_gian+=1;
+			break;
+		}	
+	}
+	
+	if (trung_gian > 0){
+		printf("Co ton tai");
+	}
+	else{
+		printf("Khong ton tai");
+	}
+	for (i=0; i<n; i++){
+		if (a[i]==m){
+			trung_gian+=1;
+}
+  }
+  printf("\n %d",a[trung_gian]);
+  printf("\n %d",a[i]);
+}
