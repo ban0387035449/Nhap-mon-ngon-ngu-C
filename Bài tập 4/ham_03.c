bd650f9ad5c848a23853f939d995141295ea3401
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2021-01-22 10:56:47 +0700

    Create ham_03.c

---
+++ Bài tập 4/ham_03.c
@@ -0,0 +1,25 @@
+//Khoi khai bao
+#include<stdio.h>
+int n,a[];
+void b(a[]);
+ //khoi ham main
+int main(){
+  int n;
+  printf("Nhap n = ");
+  scanf("%d", &n);
+  int a[n];
+  Nhap_pt_mang(a);
+  Hien_thi_pt_mang(a);
+  Hien_thi_tong_chan(a);
+  Dem_chan_le_0(a);
+  return 0;
+}
+
+// khoi dinh nghia ham
+void b(a[]){
+for(int i=0; i < n;i++){
+
+}
+
+
+  
