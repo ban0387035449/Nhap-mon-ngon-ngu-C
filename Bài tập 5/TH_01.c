9e49f20bae40b137f916d03930d24f7e4e85afc7
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2021-01-23 09:14:09 +0700

    Update TH_01.c

--- Bài tập 5/TH_01.c
+++ Bài tập 5/TH_01.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
+#include <math.h>
+int main(){
+  int a,b,c;
+  float x1,x2, delta;
+  printf("nhap a:");
+  scanf("%d",&a);
+printf("nhap b:");
+  scanf("%d",&b);
+  printf("nhap c:");
+  scanf("%d",&c);
+  delta =b*b-4*a*c;
+  if (delta<0)
+  printf("phuong trinh vo nghiem");
+  if (delta==0){
+ x1=x2=-b/(2*a);
+ printf ("x1= x2= %f",x1);
+  }
+  if(delta>0){
+    x1=(-b+ sqrt(delta))/(2*a);
+    x2=(-b- sqrt(delta))/(2*a);
+printf ("x1= %f x2=%f ",x1,x2);
+  }
+if(a-b+c==0){
+  x1=-1;
+  x2=-c/(2*a);
+  printf ("x1 = %f x2=%f ",x1,x2);
+}
+  
 
+
+
+}
