df7f75b465d03b266a5297a22d0c46a62331443f
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2021-01-23 08:49:16 +0700

    Update TH_03.c

--- Bài tập 5/TH_03.c
+++ Bài tập 5/TH_03.c
@@ -1,1 +1,30 @@
+//Khoi khai bao
+#include<stdio.h>
+void mang_a(int a,int b,int c);
 
+ //khoi ham main
+int main(){
+  int a,b,c;
+  printf("Nhap a = ");
+  scanf("%d", &a);
+  printf("Nhap b = ");
+  scanf("%d", &b);
+  printf("Nhap c = ");
+  scanf("%d", &c);
+mang_a(a,b,c);
+  return 0;
+}
+
+// khoi dinh nghia ham
+void mang_a(int a,int b,int c){
+  if(a>b &&a>c)
+   printf("%d",a);
+   
+ if (b>a&& b>c)
+ printf("%d",b);
+
+ if (c>a&&c>b)
+ printf("%d",c);
+
+  
+}
