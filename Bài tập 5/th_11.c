e169867c198800c10ea32bd5d4b8c8a270d18dff
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2021-02-27 08:01:59 +0700

    Create th_11.c

---
+++ Bài tập 5/th_11.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+  //khai báo các biến
+	float c, r,s;
+  //in  yêu cầu người dùng nhập duong kinh
+	printf("Nhap so duong kinh:");
+  	scanf("%f",&r);
+    c=r*2*3.14;
+ printf("chu vi hinh tron = %.2f",c);
+ s=r*r*3.14;
+ printf("\ndien tich hinh tron =%.2f",s);
+}
