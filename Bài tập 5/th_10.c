ac50e6f1a865fafc00f12ef46663a8ff3bb732ea
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2021-02-27 07:51:22 +0700

    Create th_10.c

---
+++ Bài tập 5/th_10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+  //khai báo các biến
+	int s, n, sum = 0;
+  //in  nap so n và yêu cầu người dùng nhập số
+	printf("Nhap so n:");
+  	scanf("%d",&n);
+  //in n= và số vừa được nhập ở trên
+	printf("n = %d\n",n);
+  //chạy vòng lặp for từ  1 đến n
+	for (s=1; s <= n; s++){
+    //tổng hiện tại ="0",sau mỗi vòng lặp cộng giá trị ="s*s"
+		sum = sum+s*s;
+	}
+  //in tong
+  printf("%d",sum);
+}
